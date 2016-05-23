7671bf5aa2a7109bfe275e05f32a0010ee9cd365
maaaaagda <magdapolak96@gmail.com>
2016-05-23 11:40:50 +0200

    With comments

--- src/Algotithms.java
+++ src/Algotithms.java
@@ -20,23 +20,32 @@ public class Algotithms {
     ArrayList<Page> Frame = new ArrayList<Page>();
     public int PF = 0;
 
-
+    /**
+     * constructor for algorithms
+     * @param FRAME_SIZE
+     * @param PagesNr
+     * @param interval
+     * @param Processes
+     */
     public Algotithms(int FRAME_SIZE, int PagesNr, int interval, int Processes) {
         this.FRAME_SIZE = FRAME_SIZE;
         this.PagesNr = PagesNr;
         this.Processes = Processes;
         this.interval = interval;
         ProcessesTab = new Proces[Processes];
+
+        //randomizing
         for (int a = 0; a < PagesNr; a++) {
             int k = (int) (Math.random() * Processes);
             int r = (int) (Math.random() * interval);
             if ((k == 3 || k == 5) && k % 3 != 0) {
                 k = 8;
             }
+            //creating page references
             PageReferences.add(new Page(r, 0, k));
 
         }
-
+    //creating table of processes
         for (int w = 0; w < Processes; w++) {
             ProcessesTab[w] = new Proces(new ArrayList(), 0);
             for (int s = 0; s < PageReferences.size(); s++) {
@@ -50,11 +59,17 @@ public class Algotithms {
 
     }
 
+    /**
+     * Equal algorithm
+     * @return global page faults
+     */
     public int EQUAL() {
-
+        //copying references
         Proces[] ProcessesTabCopy = new Proces[Processes];
         int PF_SUM = 0;
+        //frame size as a whole frames divided by processes length
         int frame_size = FRAME_SIZE / (ProcessesTab.length);
+        //adds all page faults
         for (int k = 0; k < ProcessesTab.length; k++) {
             ProcessesTabCopy[k] = new Proces(ProcessesTab[k]);
             int p = LRU(ProcessesTabCopy[k].proces, frame_size);
@@ -65,10 +80,16 @@ public class Algotithms {
         return PF_SUM;
     }
 
+    /**
+     * Proportional algorithm
+     * @return global page faults
+     */
     public int PROPORTIONAL() {
+        //frame size as a whole frames divided by processes length
         int frame_size = FRAME_SIZE / ProcessesTab.length;
         Proces[] ProcessesTabCopy = new Proces[Processes];
         int PF_SUM = 0;
+        //copying page references
         for (int k = 0; k < ProcessesTab.length; k++) {
             ProcessesTabCopy[k] = new Proces(ProcessesTab[k]);
             // ProcessesTabCopy[k].setFrame(frame_size);
@@ -76,8 +97,9 @@ public class Algotithms {
         }
         for (int j = 0; j < ProcessesTabCopy.length; j++) {
 
+            //frame size: depends on proces size
             frame_size = ProcessesTabCopy[j].proces.size() * FRAME_SIZE / PagesNr;
-
+        //minimal frame size
             if (frame_size == 0) {
                 frame_size = 3;
             }
@@ -87,9 +109,16 @@ public class Algotithms {
         return PF_SUM;
     }
 
+    /**
+     * steering page faults
+     * @return global page faults
+     */
     public int ALG_3() {
+        //max page faults for starting working algorithm
         int PFMax =(int)0.6*PagesNr;
+        //starting page size
         int frame_size = FRAME_SIZE / ProcessesTab.length;
+        //copying page rreferences
         Proces[] ProcessesTabCopy = new Proces[Processes];
         for (int k = 0; k < ProcessesTab.length; k++) {
             ProcessesTabCopy[k] = new Proces(ProcessesTab[k]);
@@ -103,7 +132,9 @@ public class Algotithms {
         while (size != 0) {
             int min = interval;
             int max = 0;
+            //index of proces which generates minimal page faults
             int minI = 0;
+            //index of proces which generates maximal page faults
             int maxI = 0;
             for (int i = 0; i < ProcessesTabCopy.length; i++) {
                 Proces t = ProcessesTabCopy[i];
@@ -113,7 +144,9 @@ public class Algotithms {
                         freeFrames = 0;
                     }
                     int pf = t.PPF;
+                    //single page fault
                     int pfsingle = t.LRU(t.proces);
+                    //changing indexes of min and max
                     if (pf > max) {
                         max = pf;
                         maxI = i;
@@ -127,6 +160,7 @@ public class Algotithms {
                 } else if (t != null) {
 
                     if (ProcessesTabCopy[maxI] != null && maxI != i) {
+                        //if process is done
                         ProcessesTabCopy[maxI].setFrame(ProcessesTabCopy[maxI].FRAME_SIZE + ProcessesTabCopy[i].FRAME_SIZE);
                     } else {
                         freeFrames += ProcessesTabCopy[i].FRAME_SIZE;
@@ -136,10 +170,13 @@ public class Algotithms {
                 }
 
             }
+            //if page faults reach edge
             if (ProcessesTabCopy[minI] != null && ProcessesTabCopy[maxI] != null && ProcessesTabCopy[minI].PFrame != 1
                     &&max>PFMax ) {
                 if (ProcessesTabCopy[minI].FRAME_SIZE > 3) {
+                    //substracting frame from proces which generates too few page faults
                     ProcessesTabCopy[minI].setFrame(ProcessesTabCopy[minI].FRAME_SIZE - 1);
+                    //adding frame for proces which generates too many page faults
                     ProcessesTabCopy[maxI].setFrame(ProcessesTabCopy[maxI].FRAME_SIZE + 1 + freeFrames);
                     freeFrames = 0;
                 }
@@ -148,25 +185,35 @@ public class Algotithms {
 
         return PFGlobal;
     }
+
+    /**
+     * zone model algotithm
+     * @param zone
+     * @return global page faults
+     */
     public int ALG_4(int zone) {
         int PFGlobal = 0;
         int freeFrames = FRAME_SIZE;
         int allDone = -1;
+        //copying page references
         Proces[] ProcessesTabCopy = new Proces[Processes];
         for (int k = 0; k < ProcessesTab.length; k++) {
             ProcessesTabCopy[k] = new Proces(ProcessesTab[k]);
 
+            //frame size as a number of duplications in zone
             int frame_size = numberOfDuplications(ProcessesTabCopy[k].proces, zone);
-
+            //setting frame size to proces
             ProcessesTabCopy[k].setFrame(frame_size);
 
 
         }
        do {
             for (int k = allDone + 1; k < ProcessesTab.length; k++) {
+                //if there are free frames
                if (freeFrames > ProcessesTabCopy[k].FRAME_SIZE) {
                     allDone++;
                    int w = ProcessesTabCopy[k].FRAME_SIZE;
+                   //substracting allocated frames
                    freeFrames-=w;
                     if(ProcessesTabCopy[k].FRAME_SIZE!= 0){
                         int h = LRU(ProcessesTabCopy[k].proces, ProcessesTabCopy[k].FRAME_SIZE);
@@ -180,10 +227,18 @@ public class Algotithms {
             }
            freeFrames = FRAME_SIZE;
         }
+       //waiting processes
             while (allDone != Processes-1);
 
         return PFGlobal;
     }
+
+    /**
+     *
+     * @param PagesRef references
+     * @param FRAME_SIZE
+     * @return page faults
+     */
     public int LRU(ArrayList<Page> PagesRef, int FRAME_SIZE) {
         PF = 0;
         ArrayList<Page> Pages2 = new ArrayList<>();
@@ -226,6 +281,12 @@ public class Algotithms {
         return PF;
     }
 
+    /**
+     *
+     * @param a arraylist of references
+     * @param zone
+     * @return number of duplications
+     */
    public int numberOfDuplications(ArrayList<Page> a, int zone)   {
        HashSet h = new HashSet();
        if(zone>a.size())
@@ -275,3 +336,14 @@ public class Algotithms {
         return PF_SUM;
     }
  */
+/*
+pr odw
+        ramki
+strony
+
+bardzo mało i bardzo duzo
+1000 500 2000 10 odwołan
+parametr( ramka strona albo strona ramka) tyle przebiegów ile drzew, ile wartosci ile jest w lisciu, punkty pomiarowe
+strona : scolar
+1000 odwołan, 250 stron globalnie, 10 ramek
+*/
--- src/Results.java
+++ src/Results.java
@@ -5,10 +5,10 @@ public class Results {
 
     public static void main (String [] args)
     {
-        Algotithms a = new Algotithms(100, 10000, 30, 10);
-        System.out.println("Założenia symulacji: \nW systemie musi byc co najmniej 2 razy więcej wolnych ramek," +
-                "\nniż procesów(minimalna liczba wolnych ramek == 3," +
-                "\nsterowanie czestoscią błędów strony: maksymalna liczba błędów: 60% odwołan\n ");
+        Algotithms a = new Algotithms(1000, 10000, 250, 1000);
+        System.out.println("Assumption of simulation: \nIn system has to be two more free frames than processes," +
+                "(minimal free frames == 3,\n Page References:10000, Frames:100, Processes:10" +
+                "\nsteering page faults: max page faults: 60% references\n ");
         System.out.println("Equal: " + a.EQUAL());
         System.out.println("******************************************************************************************");
         System.out.println("Proportional: " + a.PROPORTIONAL());
